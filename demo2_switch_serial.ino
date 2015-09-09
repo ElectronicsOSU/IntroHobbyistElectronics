4c4f6672eac52398216e1df2eaf78ae3f73fa56b
Eric Bauer <eric.bauer55@gmail.com>
2015-09-09 10:04:53 -0400

    Fixed an error with the button's pin

--- demo2_switch_serial.ino
+++ demo2_switch_serial.ino
@@ -1,34 +1,31 @@
-
 // Pin definitions
 const int LED = 2; 
-const int pushButton = 3;
+const int pushButton = 6;
 // variables
 int buttonState = 0;
 
-// the setup routine runs once when you press reset:
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600); 
   
-  // make the on-board pushbutton's pin an input pullup:
+  // make the push button configured as a Pull DOWN
   pinMode(pushButton, INPUT);
   pinMode(LED, OUTPUT); 
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
   // read the input pin:
   buttonState = digitalRead(pushButton);
   // print out the state of the button:
+  Serial.print("The button reads: ");
   Serial.println(buttonState);
+  
   //Turn on LED if the button is on
-  if(buttonState == true){
+  if(buttonState == HIGH){
    digitalWrite(LED, HIGH); 
   } else {
     digitalWrite(LED, LOW); 
   }
-  delay(1);        // delay in between reads for stability
+  
+  delay(10);        // delay in between reads for stability
 }
-
-
-
